9eb7cd54c4f61bd7b12b2fac49782cbff067ee1b
reverse-atomic-roger <37220760+reverse-atomic-roger@users.noreply.github.com>
2024-07-25 13:59:14 +0100

    Update challenge6.py
    fixed bug with hardcoded test strings :$

--- challenge6.py
+++ challenge6.py
@@ -10,7 +10,7 @@ def string_to_bytes(string:str) -> bytes:
     return bytearray(some_bytes)
 
 def calculate_hamming(string1:str, string2:str) -> int:
-    result = bytes(a ^ b for a, b in zip(string_to_bytes(test_string1), string_to_bytes(test_string2)))
+    result = bytes(a ^ b for a, b in zip(string_to_bytes(string1), string_to_bytes(string2)))
     counter = 0
     for byte in result:
         for bit in bin(byte):
@@ -22,10 +22,22 @@ def calculate_hamming(string1:str, string2:str) -> int:
 def normalise_hamming(edit_dist:int, keysize:int) -> int:
     return edit_dist/keysize
 
-def rank_key_sizes(message:str, key_size_range) -> list:
+# def rank_key_sizes(message:str, key_size_range) -> list:
+#     normalised_distances = []
+#     for size in key_size_range:
+#         distance = normalise_hamming(calculate_hamming(message[:size], message[size:size*2]), size)
+#         result = (distance, size)                                               
+#         normalised_distances.append(result)
+    
+#     return sorted(normalised_distances, key = lambda message: message[0])
+
+def rank_key_sizes(message:str, key_size_range:range, number_of_blocks:int) -> list:
     normalised_distances = []
     for size in key_size_range:
-        distance = normalise_hamming(calculate_hamming(message[:size], message[size:size*2]), size)
+        distance = 0
+        for block in range(number_of_blocks):
+            distance += normalise_hamming(calculate_hamming(message[:size*(block+1)], message[size*(block+1):size*(block+2)]), size)
+        distance /= number_of_blocks
         result = (distance, size)                                               
         normalised_distances.append(result)
     
@@ -33,8 +45,9 @@ def rank_key_sizes(message:str, key_size_range) -> list:
                   
 def main():
     with open("6.txt") as file:
-        plaintext = file.readlines()
-    results = (rank_key_sizes(plaintext, range(2,45)))
+        cyphertext = file.readlines()
+    cyphertext = "".join(cyphertext)
+    results = (rank_key_sizes(cyphertext, range(2,45), 4))
 
     for result in results:
         print(result)
